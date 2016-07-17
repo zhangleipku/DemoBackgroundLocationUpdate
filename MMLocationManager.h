b7f20637424bd314ea76b2b56719ed65499fb178
Lei Zhang 张磊 <zhanglei.cs.pku@gmail.com>
2016-07-17 12:56:43 +0900

    iOS background location update

---
+++ MMLocationManager.h
@@ -0,0 +1,21 @@
+//
+//  MMLocationManager.h
+//  DemoBackgroundLocationUpdate
+//
+//  Created by Ralph Li on 7/20/15.
+//  Copyright (c) 2015 LJC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import CoreLocation;
+@import UIKit;
+
+@interface MMLocationManager : CLLocationManager
+
++ (instancetype)sharedManager;
+
+@property (nonatomic, assign) CGFloat minSpeed;
+@property (nonatomic, assign) CGFloat minFilter;
+@property (nonatomic, assign) CGFloat minInteval;
+
+@end
